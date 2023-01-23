ba7a7f22b8ca440fd0f8249884c0b34ef0fcfafa
zoncoen <zoncoen@gmail.com>
2023-01-23 20:36:16 +0900

    fix(yaml): enable to extract a value from a list of yaml.MapSlice

---
+++ extractor/yaml/example_yaml_test.go
@@ -0,0 +1,30 @@
+package yaml_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/goccy/go-yaml"
+	"github.com/zoncoen/query-go"
+
+	yamlextractor "github.com/zoncoen/query-go/extractor/yaml"
+)
+
+func ExampleMapSliceExtractFunc() {
+	b := []byte(`- foo: bar`)
+	var v interface{}
+	if err := yaml.UnmarshalWithOptions(b, &v, yaml.UseOrderedMap()); err != nil {
+		log.Fatal(err)
+	}
+
+	q := query.New(
+		query.CustomExtractFunc(yamlextractor.MapSliceExtractFunc(true)),
+	).Index(0).Key("FOO")
+	got, err := q.Extract(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(got)
+	// Output:
+	// bar
+}
